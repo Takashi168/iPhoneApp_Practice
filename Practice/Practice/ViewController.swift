adc09ef257ebfbd95a14bd2f1412c497a1644d49
Hideaki <takahashihideaki@takahashihideakinoMacBook-puro.local>
2019-04-29 16:21:03 +0900

    first commit

---
+++ Practice/Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by Hideaki on 2019/04/29.
+//  Copyright © 2019 test company. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
